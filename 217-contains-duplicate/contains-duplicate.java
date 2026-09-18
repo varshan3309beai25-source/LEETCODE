44882dbbc5e183c2cd12493311fa138b77d967f6
varshan3309beai25-source <varshan3309.beai25@chitkara.edu.in>
2026-09-18 22:10:26 +0530

    Time: 13 ms (98.83%) | Memory: 109.1 MB (13.39%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for(int x:nums){
+            if(!seen.add(x)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
