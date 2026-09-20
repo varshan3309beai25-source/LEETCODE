5936a167649027fbd7ade2a593c64a8af650f366
varshan3309beai25-source <varshan3309.beai25@chitkara.edu.in>
2026-09-20 23:03:31 +0530

    Time: 11 ms (16.08%) | Memory: 43.1 MB (55.16%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,17 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        Map<Character,Integer> mp = new HashMap<>();
+
+        for(char ch : s.toCharArray()) {
+            mp.put(ch,mp.getOrDefault(ch,0)+1);
+        }
+        for(char ch : t.toCharArray()) {
+            mp.put(ch,mp.getOrDefault(ch,0)-1);
+            if(mp.get(ch)<0){
+                return ch;
+            }
+        }
+        return 'a';
+
+    }
+}
