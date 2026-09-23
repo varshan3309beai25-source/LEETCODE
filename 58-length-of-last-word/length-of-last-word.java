072cf903265364fa0b0caf8ed0fb7ca1ba41386c
varshan3309beai25-source <varshan3309.beai25@chitkara.edu.in>
2026-09-23 15:27:22 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (68.59%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,6 +1,19 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] words = s.trim().split(" ");
-        return words[words.length-1].length();
+         s = s.trim();
+
+         int c = 0;
+
+         for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)==' '){
+                break;
+            }
+
+            c++;
+         }
+
+         return c;
+
+
     }
 }
