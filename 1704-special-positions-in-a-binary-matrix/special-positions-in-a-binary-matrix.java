be39587df543464797634da9b3651f55bd42c2c8
varshan3309beai25-source <varshan3309.beai25@chitkara.edu.in>
2026-09-26 17:34:39 +0530

    Time: 2 ms (97.72%) | Memory: 47.3 MB (32.79%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
@@ -5,36 +5,29 @@ class Solution {
         int m = mat.length;
         int n = mat[0].length;
 
+        int[] rowc = new int[m];
+        int[] colc = new int[n];
+        
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+
+                if(mat[i][j] ==1){
+                    rowc[i]++;
+                    colc[j]++;
+                }
+            }
+        }
+
         int res = 0;
 
-        for(int row = 0;row<m;row++){
-            for(int col = 0;col<n;col++){
+        for(int r=0;r<m;r++){
+            for(int c=0;c<n;c++){
 
-                if(mat[row][col] == 0) continue;
-
-                //column check
-
-                boolean f = true;
-
-                for(int r = 0;r<m;r++){
-                    if(r!=row && mat[r][col] ==1){
-                        f = false;
-                        break;
-                    }
-                }
-
-                // row check
-
-                for(int c = 0;c<n;c++){
-                    if(c!=col && mat[row][c]==1){
-                        f = false;
-                        break;
-                    }
-                }
-
-
-                if(f == true){
+                if(mat[r][c] == 1){
+                    if(rowc[r]== 1 && colc[c] == 1){
                     res++;
+                }
                 }
             }
         }
